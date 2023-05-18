7cf5b233fd458fe807a88dc9909153b067e0a3e2
LxxsCLN <113736900+LxxsCLN@users.noreply.github.com>
2023-05-18 12:45:45 -0600

    add image assets, create data file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { albums } from './data';
+
+const image = albums[0]
 
 function App() {
+  console.log(image)
   return (
     <div className="App">
-      
+      <p>hi</p>
+      {albums.map((item, index) => {
+        return <img alt='' src={`${process.env.PUBLIC_URL}`+item.img} key={item.id}></img>
+      })}      
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,152 @@
+export const albums = [
+    {
+      id: 1,
+      img:
+        "/assets/radiohead-ok-computer.jpg",
+      text: "Radiohead - OK Computer",
+    },
+    {
+    id: 2,
+    img:
+        "/assets/radiohead-kid-a.jpg",
+    text: "Radiohead - Kid A",
+    },
+    {
+    id: 3,
+    img:
+        "/assets/radiohead-in-rainbows.jpg",
+    text: "Radiohead - In Rainbows",
+    },
+    {
+    id: 4,
+    img:
+        "/assets/daft-punk-homework.jpg",
+    text: "Daft Punk - Homework",
+    },
+    {
+    id: 5,
+    img:
+        "/assets/daft-punk-discovery.jpg",
+    text: "Daft Punk - Discovery",
+    },
+    {
+    id: 6,
+    img:
+        "/assets/daft-punk-human-after-all.jpg",
+    text: "Daft Punk - Human After All",
+    },
+    {
+    id: 7,
+    img:
+        "/assets/daft-punk-tron-legacy.jpg",
+    text: "Daft Punk - TRON: Legacy",
+    },
+    {
+    id: 8,
+    img:
+        "/assets/daft-punk-random-access-memories.jpg",
+    text: "Daft Punk - Random Access Memories",
+    },
+    {
+    id: 9,
+    img:
+        "/assets/eminem-the-slim-shady-lp.jpg",
+    text: "Eminem - The Slim Shady LP",
+    },
+    {
+    id: 10,
+    img:
+        "/assets/eminem-the-marshall-mathers-lp.jpg",
+    text: "Eminem - The Marshall Mathers LP",
+    },
+    {
+    id: 11,
+    img:
+        "/assets/eminem-the-eminem-show.jpg",
+    text: "Eminem - The Eminem Show",
+    },
+    {
+    id: 12,
+    img:
+        "/assets/metric-fantasies.jpg",
+    text: "Metric - Fantasies",
+    },
+    {
+    id: 13,
+    img:
+        "/assets/metric-synthetica.jpg",
+    text: "Metric - Synthetica",
+    },
+    {
+    id: 14,
+    img:
+        "/assets/metric-art-of-doubt.jpg",
+    text: "Metric - Art of Doubt",
+    },
+    {
+    id: 15,
+    img:
+        "/assets/metric-formentera.jpg",
+    text: "Metric - Formentera",
+    },
+    {
+    id: 16,
+    img:
+        "/assets/drake-more-life.jpg",
+    text: "Drake - More Life",
+    },
+    {
+    id: 17,
+    img:
+        "/assets/drake-views.jpg",
+    text: "Drake - Views",
+    },
+    {
+    id: 18,
+    img:
+        "/assets/drake-scorpion.jpg",
+    text: "Drake - Scorpion",
+    },
+    {
+    id: 19,
+    img:
+        "/assets/drake-dark-lane-demo-tapes.jpg",
+    text: "Drake - Dark Lane Demo Tapes",
+    },
+    {
+    id: 20,
+    img:
+        "/assets/drake-certified-lover-boy.jpg",
+    text: "Drake - Certified Lover Boy",
+    },
+    {
+    id: 21,
+    img:
+        "/assets/drake-21-savage-her-loss.jpg",
+    text: "Drake & 21 Savage - Her Loss",
+    },
+    {
+    id: 22,
+    img:
+        "/assets/king-von-grandson-vol-1.jpg",
+    text: "King Von - Grandson, Vol. 1",
+    },
+    {
+    id: 23,
+    img:
+        "/assets/king-von-levon-james.jpg",
+    text: "King Von - LeVon James",
+    },
+    {
+    id: 24,
+    img:
+        "/assets/king-von-welcome-to-oblock.jpg",
+    text: "King Von - Welcome to O'Block",
+    },
+    {
+    id: 25,
+    img:
+        "/assets/king-von-what-it-means-to-be-king.jpg",
+    text: "King Von - What It Means To Be King",
+    }
+]
